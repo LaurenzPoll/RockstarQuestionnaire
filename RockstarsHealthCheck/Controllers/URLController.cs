f65f826c640b9c2ea5cdcdfdad59053c130cfcb6
EvalynnLuna <98054728+EvalynnLuna@users.noreply.github.com>
2022-09-21 13:04:39 +0200

    URL link

---
+++ RockstarsHealthCheck/Controllers/URLController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RockstarsHealthCheck.Controllers
+{
+    public class URLController : Controller
+    {
+        public IActionResult Index(int questionaireID)
+        {
+            Console.WriteLine(questionaireID);
+            return Redirect(".../Questionaire/Index/" + questionaireID.ToString());
+        }
+    }
+}
