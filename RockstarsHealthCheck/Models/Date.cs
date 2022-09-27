0a108853e5336d56b6f55fb95c1dcc47b3ced555
Loes Reyniers <490511@student.fontys.nl>
2022-09-27 09:47:48 +0200

    Latest date and questionaire both work

--- RockstarsHealthCheck/Models/Date.cs
+++ RockstarsHealthCheck/Models/Date.cs
@@ -7,19 +7,19 @@ namespace RockstarsHealthCheck.Models
     {
         private string LatestDateTime;
 
+        private string connectionString = @"Server=tcp:rockstars.database.windows.net,1433;Initial Catalog=RockstarsDataBase;Persist Security Info=False;User ID=RockstarAdmin;Password=Rockstars!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
         public string latestDateTime { get { return LatestDateTime; } }
         public DateTime checkpoint { get; set; }
 
 
         public void DateTimeDataBase()
         {
-            var connectionString = @"Server=tcp:rockstars-health-check-server.database.windows.net,1433;Initial Catalog=rockstars-health-check-database;Persist Security Info=False;User ID=rockstars-health-check-server-admin@rockstars-health-check-server.database.windows.net;Password=6BQX5BZ0UN07G2V6$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
             using var connection = new SqlConnection(connectionString);
 
             connection.Open();
 
-            var command = new SqlCommand("INSERT INTO TestTable(DateTime) VALUES( '" + checkpoint + " ' )", connection);
+            var command = new SqlCommand("INSERT INTO HelloWorld(DateTime) VALUES( '" + checkpoint.ToString() + " ' )", connection);
             var reader = command.ExecuteReader();
 
             connection.Close();
@@ -27,8 +27,6 @@ namespace RockstarsHealthCheck.Models
 
         public void GetLatestDate()
         {
-            var connectionString = @"Server=tcp:rockstars-health-check-server.database.windows.net,1433;Initial Catalog=rockstars-health-check-database;Persist Security Info=False;User ID=rockstars-health-check-server-admin@rockstars-health-check-server.database.windows.net;Password=6BQX5BZ0UN07G2V6$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
             using var connection = new SqlConnection(connectionString);
 
             connection.Open();
@@ -38,7 +36,7 @@ namespace RockstarsHealthCheck.Models
 
             if(reader.Read())
             {
-                LatestDateTime = reader.GetString(0);
+                LatestDateTime = reader.GetString(1);
             }
 
             connection.Close();
