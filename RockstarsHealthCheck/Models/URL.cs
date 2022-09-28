ea023b94be9e5b466ad5fd08b985482038209eef
EvalynnLuna <98054728+EvalynnLuna@users.noreply.github.com>
2022-09-28 10:44:51 +0200

    Basic URL

---
+++ RockstarsHealthCheck/Models/URL.cs
@@ -0,0 +1,13 @@
+﻿namespace RockstarsHealthCheck.Models
+{
+    public class URL
+    {
+        int questionaireID = 1;
+
+        string GenerateURL(int questionaireID)
+        {
+            return "https://rockstarshealthcheck.azurewebsites.net/questionaire/" + questionaireID.ToString();
+        }
+    }
+}
+
