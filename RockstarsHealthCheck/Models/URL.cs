ea609be5987aaec6355564d924a93a409a5d817e
EvalynnLuna <98054728+EvalynnLuna@users.noreply.github.com>
2022-09-28 10:51:14 +0200

    Naming

--- RockstarsHealthCheck/Models/URL.cs
+++ RockstarsHealthCheck/Models/URL.cs
@@ -4,7 +4,7 @@
     {
         int questionaireID = 1;
 
-        string GenerateURL(int questionaireID)
+        string GenerateQuestionaireURL(int questionaireID)
         {
             return "https://rockstarshealthcheck.azurewebsites.net/questionaire/" + questionaireID.ToString();
         }
