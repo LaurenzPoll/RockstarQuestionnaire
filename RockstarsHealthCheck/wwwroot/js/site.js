139f45fa4fe11f256267b9b71838343b376c8f3c
Bart <103577999+BartSchool@users.noreply.github.com>
2022-11-11 15:01:51 +0100

    .

--- RockstarsHealthCheck/wwwroot/js/site.js
+++ RockstarsHealthCheck/wwwroot/js/site.js
@@ -1,4 +1,94 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+let slideIndex = 1;
+let slides = document.getElementsByClassName("mySlides");
+showSlides(slideIndex);
 
-// Write your JavaScript code.
+function plus2Slides(n, id) {
+    let r = document.getElementById(id + "-div").querySelectorAll("input");
+    if (r[1].checked || r[2].checked || r[3].checked) {
+        n++;
+    }
+    showSlides(slideIndex += n);
+    fixButtons(slideIndex + (n - 1));
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    fixButtons(slideIndex + (n + 1));
+}
+
+function showSlides(n) {
+    let i;
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
+
+function showSlide(n) {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex = n;
+    slides[slideIndex - 1].style.display = "flex";
+}
+
+function fixButtons(n) {
+    let b = document.getElementsByName("button");
+    let b2 = document.getElementsByName("button2");
+    let b3 = document.getElementsByName("button2");
+    let d = document.getElementsByClassName("dot");
+
+    if (n == slides.length) {
+        b.forEach(a => a.style.display = "flex");
+        b2.forEach(a => a.style.display = "none");
+    } else {
+        b.forEach(a => a.style.display = "none");
+        b2.forEach(a => a.style.display = "flex");
+    }
+
+    for (i = 0; i < d.length; i++) {
+        d[i].style.backgroundColor = "gray";
+    }
+
+    d[slideIndex/2].style.backgroundColor = "white";
+}
+
+function ShowHideDiv(id) {
+    var r = document.getElementById(id + "-div").querySelectorAll("input");
+    var l = document.getElementById(id + "-div").querySelectorAll("label");
+
+    var checked = -1;
+    for (let i = 0; i < r.length; i++) {
+        if (r[i].checked) {
+            checked = i;
+            break;
+        }
+    }
+    for (let j = 0; j <= checked; j++) {
+        l[j].innerHTML = "★";
+    }
+    for (let j = checked + 1; j < r.length; j++) {
+        l[j].innerHTML = "☆";
+    }
+
+    fixButtons2(r)
+}
+
+function fixButtons2(r) {
+    if (slideIndex == (slides.length - 1)) {
+        for (let i = 0; i < r.length; i++) {
+            if (r[i].checked) {
+                checked = i;
+                break;
+            }
+        }
+        if (checked == 1 || checked == 2 || checked == 3) {
+            fixButtons(slides.length)
+        } else (
+            fixButtons(slideIndex)
+        )
+    }
+}
